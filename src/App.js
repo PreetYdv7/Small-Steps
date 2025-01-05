6555a60fdbad73adb942ce993b50ce4a58fdab11
Preet Yadav <133802505+PreetYdv7@users.noreply.github.com>
2025-01-05 23:23:39 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+const App = () => {
+  const step = 1;
+
+  // Define `handlePrevious` and `handleNext` as separate functions
+  const handlePrevious = () => {
+    alert("Previous");
+  };
+
+  const handleNext = () => {
+    alert("Next");
+  };
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        {/* Previous Button */}
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
+          Previous
+        </button>
+
+        {/* Next Button */}
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
